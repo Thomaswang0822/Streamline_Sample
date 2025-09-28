2a4a3e0f348afd6f11350c8e6d23534e5817c0fa
Thomaswang0822 <thomaswang0822@outlook.com>
2025-09-28 15:23:26 +0800

    upgrade to C++20 to use co_wait, co_return, etc. Fix c++20-related errors

--- src/UIData.h
+++ src/UIData.h
@@ -122,7 +122,7 @@ public:
     float                               DLSSRR_Sharpness = 0.f;
     sl::DLSSDPreset                     DLSSRR_presets[static_cast<int>(sl::DLSSMode::eCount)] = {};
     sl::DLSSDPreset                     DLSSRR_last_presets[static_cast<int>(sl::DLSSMode::eCount)] = {};
-    bool UIData::DLSSRRPresetsChanged()
+    bool DLSSRRPresetsChanged()
     {
         for (int i = 0; i < static_cast<int>(sl::DLSSMode::eCount); i++)
         {
@@ -131,7 +131,7 @@ public:
         }
         return false;
     };
-    bool UIData::DLSSRRPresetsAnyNonDefault()
+    bool DLSSRRPresetsAnyNonDefault()
     {
         for (int i = 0; i < static_cast<int>(sl::DLSSMode::eCount); i++)
         {
@@ -140,12 +140,12 @@ public:
         }
         return false;
     };
-    void UIData::DLSSRRPresetsUpdate()
+    void DLSSRRPresetsUpdate()
     {
         for (int i = 0; i < static_cast<int>(sl::DLSSMode::eCount); i++)
             DLSSRR_last_presets[i] = DLSSRR_presets[i];
     };
-    void UIData::DLSSRRPresetsReset()
+    void DLSSRRPresetsReset()
     {
         for (int i = 0; i < static_cast<int>(sl::DLSSMode::eCount); i++)
             DLSSRR_last_presets[i] = DLSSRR_presets[i] = sl::DLSSDPreset::eDefault;
